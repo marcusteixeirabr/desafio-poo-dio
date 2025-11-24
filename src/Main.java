08444a323331ed15b84e75edd9155a47cd853de4
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-11-24 13:01:40 -0300

    Adiciona as classes Mentoria e Curso e renomeia a classe App para Main.

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import com.dominio.Curso;
+import com.dominio.Mentoria;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(8); 
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição do curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição da mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria de JavaScript");
+        mentoria2.setDescricao("Descrição da mentoria de JavaScript");
+        mentoria2.setData(LocalDate.of(2024, 11, 20));
+
+        System.out.println(mentoria1);  
+        System.out.println(mentoria2);
+    }
+}
---
+++ src/com/dominio/Curso.java
@@ -0,0 +1,44 @@
+package com.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Curso{");
+        sb.append("titulo=").append(titulo);
+        sb.append(", descricao=").append(descricao);
+        sb.append(", cargaHoraria=").append(cargaHoraria);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
